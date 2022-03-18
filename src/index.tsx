195d09421feaa9c8e6d7941a865d5fd6484fe901
Yaman KATBY <m.yaman.katby@gmail.com>
2022-03-18 15:20:45 +0300

    feat: add main work code

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,122 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import React, { createContext, useContext, useMemo } from 'react';
+import { useWindowDimensions } from 'react-native';
+
+export type WindowSizeClass = string | number;
+
+export type WindowSizes = { [key in WindowSizeClass]: number };
+
+export const WindowSizeClassContext = createContext<WindowSizeClass>('');
+
+export const WindowSizesContext = createContext<WindowSizes>({});
+
+export const useWindowSizeClass = () => useContext(WindowSizeClassContext);
+
+export const useWindowSizes = () => useContext(WindowSizesContext);
+
+export interface ProviderProps {
+  windowSizes: WindowSizes;
 }
+
+export const Provider: React.FC<ProviderProps> = ({
+  windowSizes,
+  children,
+}) => {
+  const { width } = useWindowDimensions();
+
+  const windowSizeClass = useMemo(() => {
+    const keys = Object.keys(windowSizes).reverse() as WindowSizeClass[];
+    const values = Object.values(windowSizes).reverse();
+
+    return keys[values.findIndex((size) => width >= size)];
+  }, [width, windowSizes]);
+
+  return (
+    <WindowSizeClassContext.Provider value={windowSizeClass}>
+      <WindowSizesContext.Provider value={windowSizes}>
+        {children}
+      </WindowSizesContext.Provider>
+    </WindowSizeClassContext.Provider>
+  );
+};
+
+if (__DEV__) {
+  Provider.displayName = 'WindowSizeProvider';
+}
+
+export const useIsWindowSizeClassUp = (sx: WindowSizeClass) => {
+  const windowSizes = useWindowSizes();
+  const keys = useMemo(
+    () => Object.keys(windowSizes) as WindowSizeClass[],
+    [windowSizes]
+  );
+
+  const windowSizeClass = useWindowSizeClass();
+
+  return useMemo(() => {
+    return keys.indexOf(windowSizeClass) >= keys.indexOf(sx);
+  }, [sx, keys, windowSizeClass]);
+};
+
+export const useIsWindowSizeClassDown = (sx: WindowSizeClass) => {
+  const windowSizes = useWindowSizes();
+  const keys = useMemo(
+    () => Object.keys(windowSizes) as WindowSizeClass[],
+    [windowSizes]
+  );
+
+  const windowSizeClass = useWindowSizeClass();
+
+  return useMemo(() => {
+    return keys.indexOf(windowSizeClass) <= keys.indexOf(sx);
+  }, [sx, keys, windowSizeClass]);
+};
+
+export const useIsWindowSizeClassBetween = (
+  sx1: WindowSizeClass,
+  sx2: WindowSizeClass
+) => {
+  const windowSizeClassUp = useIsWindowSizeClassUp(sx1);
+  const windowSizeClassDown = useIsWindowSizeClassDown(sx2);
+
+  return useMemo(() => {
+    return windowSizeClassUp && windowSizeClassDown;
+  }, [windowSizeClassUp, windowSizeClassDown]);
+};
+
+export const useIsWindowSizeClass = (sx: WindowSizeClass) => {
+  const windowSizeClass = useWindowSizeClass();
+
+  return useMemo(() => {
+    return windowSizeClass === sx;
+  }, [windowSizeClass, sx]);
+};
+
+export const useIsWindowSizeClassNot = (sx: WindowSizeClass) => {
+  const windowSizeClass = useWindowSizeClass();
+
+  return useMemo(() => {
+    return windowSizeClass !== sx;
+  }, [windowSizeClass, sx]);
+};
+
+export const useWindowSizeClassValue = <T,>(
+  query: Partial<Record<WindowSizeClass, T>>
+) => {
+  const windowSizes = useWindowSizes();
+  const keys = useMemo(
+    () => Object.keys(windowSizes) as WindowSizeClass[],
+    [windowSizes]
+  );
+
+  const windowSizeClass = useWindowSizeClass();
+
+  return useMemo(() => {
+    const queryKeys = Object.keys(query) as WindowSizeClass[];
+
+    let nearest = windowSizeClass;
+    while (!queryKeys.includes(nearest)) {
+      nearest = keys[keys.indexOf(nearest) - 1];
+    }
+    return query[nearest];
+  }, [query, keys, windowSizeClass]);
+};
